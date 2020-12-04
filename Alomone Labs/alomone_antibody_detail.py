79a88dbaf95a1e4aab4379265c9d48ba22f44bb4
zhou <napier645649968@gmail.com>
2020-12-04 16:45:20 +0800

    test

--- Alomone Labs/alomone_antibody_detail.py
+++ Alomone Labs/alomone_antibody_detail.py
@@ -136,16 +136,14 @@ class Alomone:
         return "Alomone Labs"
 
     def catalog_number(self, html):
-        catano = (
-            html.xpath('.//span[@class="sku"][@ite' 'mprop="sku"]/text()')[0]
-            .split("In wishlist")[0]
-            .strip()
-        )
+        catano = html.xpath('.//span[@class="sku"][@ite' 'mprop="sku"]/text()')[
+            0
+        ].strip()
         return catano
 
     def product_name(self, html):
         text = html.xpath('.//h1[@id="producttitle"]//text()')
-        name = "".join(i for i in text)
+        name = "".join(i for i in text).split("In wishlist")[0].strip()
         return name
 
     def antibody_type(self, html):
