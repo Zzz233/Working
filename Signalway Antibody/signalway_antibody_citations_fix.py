0220dd1bc9c4797bd7e706b43ebca8d99c186521
zhou <napier645649968@gmail.com>
2020-12-21 15:11:52 +0800

    2020/12/21 15:11

--- Signalway Antibody/signalway_antibody_citations_fix.py
+++ Signalway Antibody/signalway_antibody_citations_fix.py
@@ -53,17 +53,6 @@ class Detail(Base):
     Crawl_Date = Column(DateTime, server_default=func.now(), nullable=True, comment="")
 
 
-class Application(Base):
-    __tablename__ = "sab_antibody_application"
-
-    id = Column(Integer, primary_key=True, autoincrement=True, comment="id")
-    Catalog_Number = Column(String(40), nullable=True, comment="")
-    Application = Column(String(1000), nullable=True, comment="")
-    Dilution = Column(String(2000), nullable=True, comment="")
-    Note = Column(String(500), nullable=True, comment="")
-    Crawl_Date = Column(DateTime, server_default=func.now(), nullable=True, comment="")
-
-
 class Citations(Base):
     __tablename__ = "sab_antibody_citations"
 
@@ -78,30 +67,6 @@ class Citations(Base):
     Crawl_Date = Column(DateTime, server_default=func.now(), nullable=True, comment="")
 
 
-class Price(Base):
-    __tablename__ = "sab_antibody_price"
-
-    id = Column(Integer, primary_key=True, autoincrement=True, comment="id")
-    Catalog_Number = Column(String(40), nullable=True, comment="")
-    sub_Catalog_Number = Column(String(40), nullable=True, comment="")
-    Size = Column(String(50), nullable=True, comment="")
-    Price = Column(String(50), nullable=True, comment="")
-    Antibody_Status = Column(String(20), nullable=True, comment="")
-    Note = Column(String(500), nullable=True, comment="")
-    Crawl_Date = Column(DateTime, server_default=func.now(), nullable=True, comment="")
-
-
-class Images(Base):
-    __tablename__ = "sab_antibody_images"
-
-    id = Column(Integer, primary_key=True, autoincrement=True, comment="id")
-    Catalog_Number = Column(String(40), nullable=True, comment="")
-    Image_url = Column(String(500), nullable=True, comment="")
-    Image_description = Column(String(1000), nullable=True, comment="")
-    Note = Column(String(500), nullable=True, comment="")
-    Crawl_Date = Column(DateTime, server_default=func.now(), nullable=True, comment="")
-
-
 engine = create_engine(
     "mysql+pymysql://root:biopicky!2019@127.0.0.1:3306/biopick?charset=utf8"
 )
@@ -110,11 +75,6 @@ session = DBSession()
 
 pool = redis.ConnectionPool(host="localhost", port=6379, decode_responses=True, db=2)
 r = redis.Redis(connection_pool=pool)
-
-"""
-TODO 修饰 modify
-url含Conjugated不要
-"""
 
 
 class Signalway(object):
@@ -130,196 +90,6 @@ class Signalway(object):
             y = x.xpath('//div[@class="detail"]')[0]
         return y
 
-    def brand(self):
-        return "Signalway Antibody"
-
-    def catalog_number(self, html):
-        catalog_number = html.xpath(".//h2/text()")[0].split("#")[1]
-        return catalog_number
-
-    def product_name(self, html):
-        product_name = html.xpath(".//h2/text()")[0].split("#")[0]
-        return product_name
-
-    def antibody_type(self, html):
-        try:
-            antibody_type = html.xpath(
-                './/span[contains(text(), "Clonality")]/following-sibling::i/text()'
-            )[0].strip()
-        except Exception:
-            return None
-        return antibody_type
-
-    def sellable(self, html):
-        try:
-            yes = (
-                html.xpath('.//span[contains(text(), "Yes")]/text()')[0].strip().lower()
-            )
-        except Exception:
-            return "no"
-        if yes == "yes":
-            return yes
-
-    def synonyms(self, html):
-        try:
-            synonyms = html.xpath(
-                './/span[@class="altive"]/following-sibling::i/text()'
-            )[0].strip()
-        except Exception:
-            return None
-        return synonyms
-
-    def application(self, html):
-        try:
-            application = html.xpath(
-                './/span[contains(text(), "Applications")]/following-sibling::i/text()'
-            )[0].strip()
-        except Exception:
-            return None
-        return application
-
-    def conjugated(self):
-        return None
-
-    def clone_number(self, html):
-        try:
-            clone_number = html.xpath(
-                './/span[contains(text(), "Clone No.")]/following-sibling::i/text()'
-            )[0].strip()
-        except Exception:
-            return None
-        return clone_number
-
-    def recombinant_antibody(self):
-        return None
-
-    def modified(self, html):
-        try:
-            modified = html.xpath(
-                './/span[contains(text(), "Modification")]/following-sibling::i/text()'
-            )[0].strip()
-        except Exception:
-            return None
-        return modified
-
-    def host_species(self, html):
-        try:
-            host_species = html.xpath(
-                './/span[contains(text(), "Host Species")]/following-sibling::i/text()'
-            )[0].strip()
-        except Exception:
-            return None
-        return host_species
-
-    def reactivity_species(self):
-        return None
-
-    def antibody_detail_url(self, url):
-        return url
-
-    def antibody_status(self):
-        return None
-
-    def price_status(self):
-        return None
-
-    def citations_status(self):
-        return None
-
-    def geneid(self, html):
-        try:
-            accessionNo = html.xpath(
-                './/span[contains(text(), "Accession No.")]/following-sibling::i//text()'
-            )
-            noText = " ".join(i for i in accessionNo)
-            if "Gene ID:" in noText:
-                geneid = noText.split("Gene ID:")[1].strip().split(" ")[0]
-            elif "Gene id:" in noText:
-                geneid = noText.split("Gene id:")[1].strip().split(" ")[0]
-            else:
-                return None
-        except Exception:
-            return None
-        return geneid
-
-    def ko_validation(self):
-        return None
-
-    def species_reactivity(self, html):
-        try:
-            species_reactivity = (
-                html.xpath(
-                    './/span[contains(text(), "Species Reactivity")]/following-sibling::i/text()'
-                )[0]
-                .strip()
-                .replace(" ", ", ")
-            )
-        except Exception:
-            return None
-        return species_reactivity
-
-    def immunogen(self, html):
-        try:
-            immunogen = html.xpath(
-                './/span[contains(text(), "Immunogen Description")]/following-sibling::i/text()'
-            )[0].strip()
-        except Exception:
-            return None
-        return immunogen
-
-    def swissprot(self, html):
-        try:
-            accessionNo = html.xpath(
-                './/span[contains(text(), "Accession No.")]/following-sibling::i//text()'
-            )
-            noText = " ".join(i for i in accessionNo)
-            if "Swiss-Prot#:" in noText:
-                swissprot = noText.split("Swiss-Prot#: ")[1].split(" ")[0]
-            elif "Swiss-Prot:" in noText:
-                swissprot = noText.split("Swiss-Prot: ")[1].split(" ")[0]
-            else:
-                return None
-        except Exception:
-            return None
-        return swissprot
-
-    def predicted_mw(self, html):
-        try:
-            predicted_mw = html.xpath(
-                './/span[contains(text(), "Calculated MW")]/following-sibling::i/text()'
-            )[0].strip()
-        except Exception:
-            return None
-        return predicted_mw
-
-    def observed_mw(self, html):
-        # SDS obs  Cal pre
-        try:
-            observed_mw = html.xpath(
-                './/span[contains(text(), "SDS-PAGE MW")]/following-sibling::i/text()'
-            )[0].strip()
-        except Exception:
-            return None
-        return observed_mw
-
-    def isotype(self, html):
-        try:
-            isotype = html.xpath(
-                './/span[contains(text(), "Isotype")]/following-sibling::i/text()'
-            )[0].strip()
-        except Exception:
-            return None
-        return isotype
-
-    def purify(self, html):
-        try:
-            purify = html.xpath(
-                './/span[contains(text(), "Purification")]/following-sibling::i/text()'
-            )[0].strip()
-        except Exception:
-            return None
-        return purify
-
     def citations(self, html):
         try:
             citations = len(
@@ -328,94 +98,6 @@ class Signalway(object):
         except Exception:
             return 0
         return citations
-
-    def citations_url(self):
-        return None
-
-    def dataSheet_url(self, html):
-        try:
-            dataSheet_url = (
-                "https://www.sabbiotech.com.cn/"
-                + html.xpath('.//a[@class="btn-pdf"]/@href')[0].strip()
-            )
-        except Exception:
-            return None
-        return dataSheet_url
-
-    def review(self):
-        return None
-
-    def price_url(self):
-        return None
-
-    def image_qty(self, html):
-        try:
-            image_qty = len(
-                html.xpath(
-                    './/span[contains(text(), "Images")]/../following-sibling::div[@class="content"]/ul/li'
-                )
-            )
-        except Exception:
-            return 0
-        return image_qty
-
-    def image_url(self):
-        return None
-
-    def Note(self):
-        return None
-
-    # ======================================================================== #
-    # application表
-    def sub_application(self, html):
-        # 使用时判断返回类型 list str
-        results = []
-        note = 0
-        try:
-            p = html.xpath(
-                './/span[contains(text(), "Application Details")]/../following-sibling::div[@class="content"]/ul/li//text()'
-            )
-        except Exception:
-            return results
-        for text in p:
-            if "Not yet tested in other applications." in text:
-                break
-            elif "\t" in text:
-                sub_app = text.split("\t")[0].replace("/n", "").strip()
-                sub_dil = text.split("\t")[1].strip()
-                # print(sub_app, sub_dil)
-                results.append([sub_app, sub_dil])
-            elif ": " in text:
-                sub_app = text.split(": ")[0].replace("/n", "").strip()
-                sub_dil = text.split(": ")[1].strip()
-                # print(sub_app, sub_dil)
-                results.append([sub_app, sub_dil])
-            elif " (" in text:
-                for single_line in text.split(","):
-                    sub_app = single_line.split(" (")[0].replace("/n", "").strip()
-                    sub_dil = single_line.split(" (")[1].replace(")", "").strip()
-                    # print(sub_app, sub_dil)
-                    results.append([sub_app, sub_dil])
-                return results
-            else:
-                note = " | ".join(i for i in p)
-                return note
-        return results
-
-    # ======================================================================== #
-    # price表
-    def sub_price(self, html):
-        results = []
-        try:
-            trs = html.xpath('.//table[@class="table table-borderless"]/tbody/tr')
-        except Exception:
-            return results
-        for item in trs:
-            sub_num = item.xpath(".//td//text()")[0].strip()
-            sub_siz = item.xpath(".//td//text()")[1].strip()
-            sub_pri = item.xpath(".//td//text()")[2].strip()
-            results.append([sub_num, sub_siz, sub_pri])
-        return results
 
     # ======================================================================== #
     # Citations表
@@ -433,53 +115,29 @@ class Signalway(object):
             return results
         return results
 
-    # ======================================================================== #
-    # images表
-    def sub_images(self, html):
-        results = []
-        try:
-            lis = html.xpath(
-                './/span[contains(text(), "Images")]/../following-sibling::div[@class="content"]/ul/li'
-            )
-        except Exception:
-            return results
-        for item in lis:
-            img_url = (
-                "https://www.sabbiotech.com.cn/"
-                + item.xpath(".//span/img/@src")[0].strip()
-            )
-            try:
-                img_des = item.xpath(".//i/text()")[0].strip()
-            except Exception:
-                img_des = None
-            results.append([img_url, img_des])
-        return results
-
 
 if __name__ == "__main__":
-    for i in range(1):
-        # while r.exists("Signalway"):
-        #     url = r.lpop("Signalway")
-        url = "https://www.sabbiotech.com.cn/g-170611-Goat-anti-Mouse-IgG-Secondary-AntibodyHRP-conjugated-L3032.html"
-        if "-Conjugated-" in url:
-            print("pass")
-            continue
-        print(url)
+    # for i in range(1):
+    while r.exists("signalway_fixer"):
+        extract = r.lpop("signalway_fixer")
+        url = extract.split(",")[0]
+        catanum = extract.split(",")[-1]
+        print(extract)
+        # url = "https://www.sabbiotech.com.cn/g-170611-Goat-anti-Mouse-IgG-Secondary-AntibodyHRP-conjugated-L3032.html"
         try:
             lxml = Signalway().format(url)
         except Exception as e:
             print(e)
-            r.rpush("Signalway", url)
+            r.rpush("signalway_fixer", extract)
             continue
 
-        brand = Signalway().brand()
-        catalog_number = "123456"
         citations = Signalway().citations(lxml)
-
+        # print(citations)
         sub_citations = Signalway().sub_citations(lxml)
-
-        new_detail = Detail()
-        session.add(new_detail)
+        # print(sub_citations)
+        session.query(Detail).filter(Detail.Catalog_Number == catanum).update(
+            {Detail.Citations: str(citations)}
+        )
 
         if isinstance(sub_citations, list):
             objects_sub_citations = []
@@ -488,7 +146,7 @@ if __name__ == "__main__":
                 tit = sub[1]
                 lin = sub[2]
                 new_citations = Citations(
-                    Catalog_Number=catalog_number,
+                    Catalog_Number=catanum,
                     PMID=pid,
                     Article_title=tit,
                     Pubmed_url=lin,
@@ -503,7 +161,7 @@ if __name__ == "__main__":
             session.close()
             print("done")
         except Exception as e:
-            r.rpush("Signalway", url)
+            r.rpush("signalway_fixer", extract)
             session.rollback()
             print(2, e)
-        time.sleep(random.uniform(1, 2.5))
+        time.sleep(random.uniform(4.5, 6.5))
