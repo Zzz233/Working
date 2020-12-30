09ee87b19b7ea25b0ef35be137e7dfc536582147
zhou <napier645649968@gmail.com>
2020-12-30 15:10:24 +0800

    2020/12/30 15:10

---
+++ Immunoway/Immunoway_kit_list.py
@@ -0,0 +1,57 @@
+import requests
+from lxml import etree
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Text, DateTime, Index
+from sqlalchemy import Column, String, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import func
+import time
+import random
+
+Base = declarative_base()
+
+
+class Data(Base):
+    __tablename__ = "immunoway_kit_list"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, comment="id")
+    Brand = Column(String(40), nullable=True, comment="")
+    Catalog_Number = Column(String(100), nullable=True, comment="")
+    Product_Name = Column(String(200), nullable=True, comment="")
+    Detail_url = Column(String(1000), nullable=True, comment="")
+    Note = Column(String(500), nullable=True, comment="")
+    Kit_Status = Column(String(20), nullable=True, comment="")
+    Crawl_Date = Column(DateTime, server_default=func.now(), nullable=True, comment="")
+
+
+engine = create_engine(
+    "mysql+pymysql://root:biopicky!2019@127.0.0.1:3306/bio_kit?charset=utf8"
+)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+brand = "Immunoway"
+
+for i in range(1, 79):
+    url = f"http://www.immunoway.com/index.php/home/product/smallclass/id/60/p/{i}.html"
+    with requests.Session() as s:
+        resp = s.get(url=url)
+        # print(resp.text)
+        lxml = etree.HTML(resp.text)
+        divs = lxml.xpath('//div[@class="list"]')
+        for item in divs:
+            link = "http://www.immunoway.com" + item.xpath(".//a/@href")[0].strip()
+            catano = item.xpath('.//li[contains(text(),"Catalog No.：")]/text()')[
+                0
+            ].replace("Catalog No.：", "")
+            print(link, catano)
+            new_data = Data(Brand=brand, Catalog_Number=catano, Detail_url=link)
+            try:
+                session.add(new_data)
+                session.commit()
+                session.close()
+            except Exception as e:
+                session.rollback()
+                print(e)
+                continue
+        print(i, "done")
+    time.sleep(random.uniform(1.0, 1.5))
